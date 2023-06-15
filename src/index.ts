be3c2e82761d627f0bb1d0e2a00cb946f3367914
Bluzzi <camilledugas.pro@gmail.com>
2023-06-15 02:48:33 +0200

    feat(index.ts): add functionality to open first direct message, get username of first friend, and send a message to them.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,23 @@ const run = async(): Promise<void> => {
 
   await loginDiscord(page);
 
-  await page.waitForURL("https://discord.com/app");
+  // Open the first direct message:
+  const directMessages = page.getByRole("link", { name: "direct message" });
+  const firstFriend = directMessages.nth(0);
+
+  await directMessages.nth(0).click();
+
+  // Get username of the first friend:
+  const username = await firstFriend.textContent();
+
+  // Send a message:
+  const textbox = page.getByRole("textbox", { name: "Message" });
+
+  await textbox.type(`Hello ${username || ""}!`);
+  await page.keyboard.press("Enter");
+
+  await textbox.type("How are you today?");
+  await page.keyboard.press("Enter");
 };
 
 void run();
